3016823de241e89d6a01b120949ed3d72b44c00c
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2025-10-24 21:32:49 +0100

    ✨feat: FP07

---
+++ src/structures/FP07/Main.java
@@ -0,0 +1,53 @@
+package structures.FP07;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         # Parte I
+         1. Escrever um método recursivo q imprima tds os elems de uma LL, do 1o ao último elem. Deverá ser possível usar o método pra qlqr LL à sua escolha. Testar a sol desenvolvida.
+         */
+
+
+
+        /**
+         * 2. Escrever 2 métodos recursivos q imprima tds os elems de uma DLL, do 1o ao último elem. Um dos métodos deverá imprimir a partir de uma ponta e o outro de outra. Deverá ser possível usar os métodos pra quaisquer DLL's à sua escolha. Testar a sol desenvolvida.
+         */
+
+
+
+        /**
+         * 3. Desenvolver um programa pra a resolução dos problemas das Torres de Hanoi apresentado no slide 16. Demostra a sua utilização.
+         */
+
+
+
+        /**
+         * Parte II
+         * 1. Quais as componentes q compõem uma definição recursiva?
+         */
+
+
+
+        /**
+         * 2. Devo usar smp a recursividade pra resolver os + diversos problemas?
+         */
+
+
+
+        /**
+         * 3. Acrescentar um método recursivo à implementação de DLL q devolva o conteúdo da list c a ordem dos elems invertida.
+         */
+
+
+
+        /**
+         * 4. Acrescentar o método void replace(T existingElem, T newElem), q funcione de forma recursiva, à sua implementação de LL q seja capaz de substituir tds as ocorrências do argumento existingElem pelo argumento newElem.
+         */
+
+
+        /**
+         * 5. Acrescentar um método recursivo à sua implementação de LL q seja capaz de inverter a ordem dos elems.
+         */
+        
+    }
+}
